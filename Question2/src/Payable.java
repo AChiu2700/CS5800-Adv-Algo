ec9e27907082fe2cb4b83485e849574336bfca41
AChiu2700 <43284519+AChiu2700@users.noreply.github.com>
2025-09-12 12:43:08 -0700

    Delete Question2 directory

--- Question2/src/Payable.java
+++
@@ -1,8 +0,0 @@
-public interface Payable {
-    double calculatePayment();
-    String getPayeeName();
-    void print();
-    // public static void main(String[] args) throws Exception {
-    //     System.out.println("Hello, World!");
-    // }
-}
